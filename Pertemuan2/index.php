b1aff1810dc9439a3f9270fbf8455eacdc9493cb
Dik-Dik-Nur-Illahi <dikdiknurillahi17@gmail.com>
2021-03-01 21:27:39 +0700

    Revert " Unpdate Folder Menjadi Kelas dan Praktikum dan Menyelesaikan Pertemuan 4"
    This reverts commit f453c0e3b3c7ff39bd16281ba7669a8869af8d76.

---
+++ Pertemuan2/index.php
@@ -0,0 +1,90 @@
+<?php
+/*
+Dik Dik Nur Illahi
+203040072
+https://github.com/Dik-Dik-Nur-Illahi/pw2021_203040072
+Pertemuan 2 - 14 Februari 2021
+Mempelajari Mengenai sintaks PHP
+*/
+?>
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan tipe Data 
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Dik Dik Nur Illahi";
+// echo "Halo, nama saya $nama";
+
+// Operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung sting / concatenation / concat
+// .
+// $nama_depan = "Dik Dik";
+// $nama_belakang = "Nur Illahi";
+// echo $nama_depan . " " . $nama_belakang
+
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Dik Dik";
+// $nama .= " ";
+// $nama .= "Nur Illahi";
+// echo $nama
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 < 5);
+
+// identitas
+// ===, !==
+// var_dump(1 === 5);
+
+// Logika
+// &&, ||, !
+// $x = 30
+// var_dump($x < 20 || $x % 2 == 0);
+
+
+
+
+
+
+
+?>
+<!-- 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP </title>
+</head>
+
+<body>
+    <h1>Halo, Selamat Datang <?php echo $nama; ?></h1>
+    <p> <?php echo "ini adalah paragraf"; ?> </p>
+
+</body>
+
+</html> -->
