47ce8357aaa86975f9dd1dfbf062a46a68119cbe
Dik-Dik-Nur-Illahi <dikdiknurillahi17@gmail.com>
2021-03-13 00:28:54 +0700

    menyelesaikan pertemuan 4

---
+++ Pertemuan6/Latihan1.php
@@ -0,0 +1,87 @@
+<?php
+/*
+Dik Dik Nur Illahi
+203040072
+https://github.com/Dik-Dik-Nur-Illahi/pw2021_203040072
+Pertemuan 6 - 12 Maret 2021
+Mempelajari Mengenai Array Associative
+*/
+?>
+
+<?php 
+// array
+// Membuat Array
+
+// Ada 2 cara membuat array cara lama dan cara baru
+
+// cara lama
+$hari = array("Senin", "Selasa", "Rabu");
+// cara baru
+$bulan = ["Januari", "Februari", "Maret"];
+$arr = [100, "teks", true];
+// Menampilkan array
+// versi debugging
+var_dump($hari);
+echo "<br>";
+print_r($bulan);
+echo "<br>";
+echo "<br>";
+// menampilkan 1 elemen array
+echo $arr[0];
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Array</title>
+    <style>
+        .kotak {
+            width: 30px;
+            height: 30px;
+            background-color: #BADA55;
+            text-align: center;
+            line-height: 30px;
+            margin: 3px;
+            float: left;
+            transition: 1s;
+        }
+        .kotak:hover{
+            transform: rotate(360deg);
+            border-radius: 50%;
+        }
+        .clear {
+            clear: both;
+        }
+    </style>
+</head>
+<body>
+    
+    <?php 
+    $angka =[
+            [1,2,3],
+            [4,5,6],
+            [7,8,9]
+]; // Array Multi dimensi
+    // echo $angka [1][1];
+    // echo $angka [2][2];
+    ?>
+
+
+
+    <?php foreach( $angka as $a ) : ?>
+        <?php foreach( $a as $b ) : ?>
+            <div class="kotak"><?= $b; ?> </div>
+        <?php endforeach ; ?>
+        <div class="clear"></div>
+    <?php endforeach; ?>
+
+</body>
+</html>
---
+++ Pertemuan6/Latihan2.php
@@ -0,0 +1,98 @@
+<?php
+/*
+Dik Dik Nur Illahi
+203040072
+https://github.com/Dik-Dik-Nur-Illahi/pw2021_203040072
+Pertemuan 6 - 12 Maret 2021
+Mempelajari Mengenai Array Associative
+*/
+?>
+
+<?php 
+$mahasiswa1 = [
+["Dik Dik Nur Illahi", "203040072", "dikdiknurillahi@gamil.com", "Teknik Informatika"],
+["Uciha Boruto", "203040069", "ucihaboruto@gamil.com", "Teknik Ninja"]
+]; 
+
+
+// Array Associative
+// definisi sama seperti array numerik, kecuali
+// key-nya adalah string yang kita buat sendiri
+$mahasiswa = [
+    [
+    "nama" => "Dik Dik Nur Illahi",
+    "nrp" => "203040072",
+    "email" => "dikdiknurillahi@gmail.com",
+    "jurusan" => "Teknik Informatika",
+    "gambar" => "1.jpg"
+],
+    [
+    "nama" => "Uciha Boruto",
+    "nrp" => "203040069",
+    "email" => "ucihaboruto@gmail.com",
+    "jurusan" => "Teknik Ninja",
+    "gambar" => "boruto.jpg"
+]
+];
+// echo $mahasiswa[1]["tugas"][1];
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Mahasiswa</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+
+    <?php foreach ($mahasiswa as $mhs ) : ?>
+    <ul>
+        <li>
+            <img src="img/<?= $mhs["gambar"]; ?>">
+        </li>
+        <li>Nama : <?= $mhs["nama"]; ?> </li>
+        <li>NRP : <?= $mhs["nrp"]; ?> </li>
+        <li>Email : <?= $mhs["email"]; ?> </li>
+        <li>Jurusan : <?= $mhs["jurusan"]; ?> </li>
+    </ul>
+    <?php endforeach; ?>
+
+</body>
+</html>
+
+<?= "<br>" ?>
+<?= "<br>" ?>
+<?= "<br>" ?>
+<?= "<br>" ?>
+<?= "<br>" ?>
+<?= "<br>" ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Mahasiswa</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+
+    <?php foreach ($mahasiswa1 as $mhs ) : ?>
+    <ul>
+        <li>Nama : <?= $mhs[0]; ?> </li>
+        <li>NRP : <?= $mhs[1]; ?> </li>
+        <li>Email : <?= $mhs[2]; ?> </li>
+        <li>Jurusan : <?= $mhs[3]; ?> </li>
+    </ul>
+    <?php endforeach; ?>
+
+</body>
+</html>
